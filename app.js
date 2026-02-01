a5bcaa33c22eb2754b49ffff26e1609052a7f5ad
MarryJoia <140195349+MarryJoia@users.noreply.github.com>
2026-02-01 10:50:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// 🔥 Firebase Config
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyCHX45QbjATYaI5yO50ghgSoZP98yXo3Hs",
+  authDomain: "earning-platform-a267f.firebaseapp.com",
+  databaseURL: "https://earning-platform-a267f-default-rtdb.firebaseio.com",
+  projectId: "earning-platform-a267f",
+  storageBucket: "earning-platform-a267f.firebasestorage.app",
+  messagingSenderId: "785014377238",
+  appId: "1:785014377238:web:cbdbf4209286465f1c1e37",
+  measurementId: "G-Z3CGN0WMVF"
 };
 
 firebase.initializeApp(firebaseConfig);
